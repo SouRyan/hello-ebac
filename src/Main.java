99e0739b3a4e052f5b967978121245cdbf3ac99f
SouRyan <ryanbrayan.fs@gmail.com>
2026-09-24 16:51:37 -0300

    atividade de inserir dados

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    IO.println(String.format("Hello and welcome!"));
+    Scanner scanner = new Scanner(System.in);
+    IO.println(String.format("Olá usuario, para começarmos, insira seu nome"));
+    String nome = scanner.nextLine();
+
+    IO.println(String.format("Agora, insira seu cpf"));
+    String cpf = scanner.nextLine();
+
+    IO.println(String.format("insira sua idade"));
+    int idade = scanner.nextInt();
+
+    IO.println(String.format("insira sua altura em metros, utilize virgula"));
+    double altura = scanner.nextDouble();
+
+    IO.println(String.format("insira sua estado civil"));
+    String Civil = scanner.nextLine();
+
+    IO.println("Seja bem vindo " + nome + " ! seu CPF é: " + cpf + " você tem " + idade + " com essa altura: " + altura+ "metros, parabens!");
+
+
 
 }
